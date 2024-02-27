6244db6445716d5cfa4c838354e335135fbe3324
HyoEun Sim <hyoeun5000@naver.com>
2024-02-27 13:38:39 +0900

    ✨feat: prisma service / modules 생성 및 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./domains/auth/auth.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
