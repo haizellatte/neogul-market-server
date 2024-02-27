4556b82b9df6545aae2b31f4332e09e04aabb9a9
HyoEun Sim <hyoeun5000@naver.com>
2024-02-27 13:31:41 +0900

    chore: create folders

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
+const cors_1 = require("cors");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, {
+        cors: true,
+    });
     await app.listen(5050);
+    app.useGlobalPipes(new common_1.ValidationPipe());
+    app.use((0, cors_1.default)());
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import cors from "cors";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<INestApplication>(AppModule, {
+    cors: true,
+  });
   await app.listen(5050);
+  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalInterceptors(new TransformInterceptor());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(cors());
 }
 bootstrap();
