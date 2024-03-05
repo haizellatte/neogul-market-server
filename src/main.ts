7a207009c369680c4a1cb6c460a43375f3aa2339
HyoEun Sim <hyoeun5000@naver.com>
2024-03-05 11:53:29 +0900

    install cors & app.use : core

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { INestApplication, ValidationPipe } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { HttpExceptionFilter } from "./filters/http.exception.filter";
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as cors from 'cors';
+import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http.exception.filter';
 
 async function myServer() {
   const app = await NestFactory.create<INestApplication>(AppModule, {
     cors: true,
   });
+
+  app.use(cors());
   await app.listen(5050);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
